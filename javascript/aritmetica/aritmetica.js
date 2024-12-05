569d3a7ce4ce414bdda86070e73a2f687da169df
Nogyboy <guamanc9@gmail.com>
2024-12-05 15:49:10 -0500

    docs: agregar aritmetica

---
+++ javascript/aritmetica/aritmetica.js
@@ -0,0 +1,38 @@
+// Enteros
+let num = 10 * 5;
+log("Multiplicación", num);
+
+let num2 = 10 + 5;
+log("Suma", num2);
+
+let num3 = 10 - 5;
+log("Resta", num3);
+
+let num4 = 10 / 5;
+log("División", num4);
+
+let num5 = 10 % 5;
+log("Módulo", num5);
+
+
+console.log("-------- Decimales --------");
+// Decimales
+let num6 = 10.52 * 5;
+log("Multiplicación", num6);
+
+let num7 = 10.5 + 5.52;
+log("Suma", num7);
+
+let num8 = 10.5 - 5.098;
+log("Resta", num8);
+
+let num9 = 10.599 / 5;
+log("División", num9);
+
+let num10 = 10.5 % 5;
+log("Módulo", num10);
+
+
+function log(msg, num) {
+    console.log(msg, num);
+}
