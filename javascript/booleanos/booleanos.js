bae145d27d12eb561a3d79c5f4349e9c44d91b3a
Nogyboy <guamanc9@gmail.com>
2024-12-05 16:24:41 -0500

    docs: tipos booleanos

---
+++ javascript/booleanos/booleanos.js
@@ -0,0 +1,11 @@
+const bool1 = true;
+const bool2 = false;
+
+console.log(bool1);
+console.log(bool2);
+console.log(typeof bool1);
+
+// Una forma de producir un valor booleano es usar la comparación
+console.log(10 > 5);
+
+console.log("Aardvark" < "Zoroaster")
---
+++ javascript/booleanos/booleanos.md
@@ -0,0 +1,7 @@
+# Booleanos
+Booleanos son valores que pueden ser verdadero o falso. Son valores lógicos.
+
+```js
+let bool1 = true;
+let bool2 = false;
+```
